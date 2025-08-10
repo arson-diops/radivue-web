825e81646b4c7f5f78ee16539cb11e29946f886b
Benjamin Durange <bdurange0525@gmail.com>
2025-08-10 11:15:47 -0400

    Fix bucket path in wrangler.toml and update event handling in index.js

--- workers-site/index.js
+++
@@ -1,8 +0,0 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  // Serve static assets from the public/ directory
-  return await fetch(request)
-}
